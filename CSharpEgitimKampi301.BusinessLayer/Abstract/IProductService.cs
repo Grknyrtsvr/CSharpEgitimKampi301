aa286bcfee9cfb8dd3f40e9ebd42047d0c29c776
Grknyrtsvr <grknyrtsvr@hotmail.com>
2024-12-17 10:03:34 +0300

    ...

--- CSharpEgitimKampi301.BusinessLayer/Abstract/IProductService.cs
+++ CSharpEgitimKampi301.BusinessLayer/Abstract/IProductService.cs
@@ -5,9 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using CSharpEgitimKampi301.EntityLayer.Concrete;
 
-namespace CSharpEgitimKampi301.DataAccessLayer.Abstract
+namespace CSharpEgitimKampi301.BusinessLayer.Abstract
 {
-    public interface IPoductDal:IGenericDal<Product>
+    public interface IProductService : IGenericService<Product>
     {
+        List<object> TGetProductsWithCategory();
     }
 }
